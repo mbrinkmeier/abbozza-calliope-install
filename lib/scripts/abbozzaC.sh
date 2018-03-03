027ef8799a2d7e4c1e74b3c1eb12d2a7c4d3152f
brinkmeier <brinkmeier@famuluss-mbp.fritz.box>
2018-03-03 22:30:22 +0100

    * MAC OS: Compile path corrected

--- lib/scripts/abbozzaC.sh
+++ lib/scripts/abbozzaC.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+
+export PATH=/usr/local/bin:$PATH
+
 java -cp ../lib/jssc-2.8.0.jar:../lib/rsyntaxtextarea.jar:../lib/autocomplete.jar:../lib/abbozza-calliope.jar:..\lib\commons-io-2.5.jar de.uos.inf.did.abbozza.calliope.AbbozzaCalliopeC
