3d144d522a5175f7bb980ff9ef857db4b08ede88
michael <michael@ridcully>
2018-06-06 19:35:16 +0200

    * Added check for integrated tools in start script fro linmux and mac

--- lib/scripts/abbozzaC.sh
+++ lib/scripts/abbozzaC.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-export PATH=/usr/local/bin:$PATH
+# The script has to be executed from the bin directory.
+
+# Check if ../tools exists
+if [ -d ../tools ]; then
+  # Setting PATH
+  export PATH=`pwd`/../cwtoolslinux/bin:`pwd`/../bin:$PATH
+
+  # Activate virtualenv
+  cd cwtoolslinux
+  . bin/activate
+
+  # start java
+  cd ../bin
+fi
 
 java -cp ../lib/jssc-2.8.0.jar:../lib/rsyntaxtextarea.jar:../lib/autocomplete.jar:../lib/abbozza-calliope.jar:..\lib\commons-io-2.5.jar de.uos.inf.did.abbozza.calliope.AbbozzaCalliopeC
