4c3d1f88ffb2b8d21bfbffccfa1afa64b2244559
michael <michael@DESKTOP-QS16JJM.fritz.box>
2018-06-17 10:44:10 +0200

    * Switched to javaw

--- lib/scripts/abbozzaC.sh
+++ lib/scripts/abbozzaC.sh
@@ -15,4 +15,4 @@ if [ -d ../tools ]; then
   cd ../bin
 fi
 
-java -cp ../lib/jssc-2.8.0.jar:../lib/rsyntaxtextarea.jar:../lib/autocomplete.jar:../lib/abbozza-calliope.jar:..\lib\commons-io-2.5.jar de.uos.inf.did.abbozza.calliope.AbbozzaCalliopeC
+javaw -cp ../lib/jssc-2.8.0.jar:../lib/rsyntaxtextarea.jar:../lib/autocomplete.jar:../lib/abbozza-calliope.jar:..\lib\commons-io-2.5.jar de.uos.inf.did.abbozza.calliope.AbbozzaCalliopeC
